5d25e7d8bfd0b304fd701aaad064a48e61726cf6
Brooskiey BaKiser <brooskiey17@gmail.com>
2023-09-28 21:01:43 -0400

    Initial Commit

---
+++ src/main/java/dev/brooskiey/personalplanner/PersonalPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.brooskiey.personalplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPlannerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalPlannerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/brooskiey/personalplanner/PersonalPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brooskiey.personalplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPlannerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
